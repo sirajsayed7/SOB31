af9f3d1a2f8185576160513f054aadd76b2abf69
sirajsayed7 <sirajsayed7@gmail.com>
2021-11-20 16:59:18 +0400

    files fixed

--- broken_grades.py
+++ broken_grades.py
@@ -21,36 +21,33 @@
 
 exam_one = int(input("Input exam grade one: "))
 
-exam_two = input("Input exam grade two: "))
+exam_two = int(input("Input exam grade two: "))
 
-exam_3 = str(input("Input exam grade three: "))
+exam_three = int(input("Input exam grade three: "))
 
-grades = [exam_one exam_two exam_three]
-sum = 0
-for grade in grade:
-  sum = sum + grade
+grades = [exam_one, exam_two, exam_three]
+total = 0
+for i in grades:
+    total = exam_one + exam_two + exam_three
+avg = total / len(grades)
 
-avg = sum / len(grdes)
 
-if avg >= 90:
+if avg > 89:
     letter_grade = "A"
-elif avg >= 80 and avg < 90
+elif avg >= 80 and avg < 90:
     letter_grade = "B"
-elif avg > 69 and avg < 80:
-    letter_grade = "C'
-elif avg <= 69 and avg >= 65:
+elif avg >= 70 and avg < 80:
+    letter_grade = "C"
+elif avg >= 60 and avg < 70:
     letter_grade = "D"
-elif:
+else:
     letter_grade = "F"
 
-for grade in grades:
-    print("Exam: " + str(grade))
+print("Exam: ", (grades))
+print("Average: ", round(float((avg))))
+print("Grade: ", str((letter_grade)))
 
-    print("Average: " + str(avg))
-
-    print("Grade: " + letter_grade)
-
-if letter-grade is "F":
-    print "Student is failing."
+if letter_grade == "F":
+    print("Student is failing.")
 else:
-    print "Student is passing."
+    print("Student is passing.")
